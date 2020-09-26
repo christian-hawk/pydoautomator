08de35f5826d0de405d60cfceba53281036ad918
Christian Eland <eland.christian@gmail.com>
2020-09-26 09:04:16 -0300

    initial

--- tests/behaver/features/environment.py
+++ tests/behaver/features/environment.py
@@ -1,6 +1,11 @@
 # -- FILE:features/environment.py
+import os
+
+OD_TOKEN = os.getenv('OD_TOKEN')
+
 
 def before_all(context):
-    context.token = '2431074b5dd96c51d3428a78496dc9c7d184ec0db150cd954483de4955e28890'
+    context.token = OD_TOKEN
+    #context.token = '2431074b5dd96c51d3428a78496dc9c7d184ec0db150cd954483de4955e28890'
     context.headers = {'Authorization': 'Bearer ' + context.token}
     context.api_uri = 'https://api.digitalocean.com/v2'
--- tests/unit_tdd/test_package_basics.py
+++ tests/unit_tdd/test_package_basics.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 import importlib
+import pydoautomator
+import inspect
 
 
 class TestPackageBasics(TestCase):
@@ -7,3 +9,32 @@ class TestPackageBasics(TestCase):
 
         self.assertIsNotNone(importlib.find_loader(
             'pydoautomator'), 'module does not exist!')
+
+    def test_if_Droplet_exists_in_package(self):
+        self.assertTrue(
+            hasattr(pydoautomator, 'Droplet'),
+            'Droplet does not exists in package'
+        )
+
+    def test_Droplet_should_be_not_none(self):
+        self.assertIsNotNone(
+            pydoautomator.Droplet,
+            'Droplet has None value!'
+        )
+
+    def test_if_Droplet_is_a_class(self):
+        self.assertTrue(
+            inspect.isclass(pydoautomator.Droplet),
+            'pydoautomator.Droplet is not a class'
+        )
+
+    def test_if_Droplet_is_from_droplet_submodule(self):
+        self.assertTrue(
+            pydoautomator.Droplet.__module__ == 'pydoautomator.droplet',
+            'pydoautomator.Droplet is NOT a module from pydoautomator.droplet'
+        )
+
+    def test_create_droplet_should_exist(self):
+        self.assertTrue(
+            hasattr(pydoautomator.Droplet, 'create_droplet')
+        )
